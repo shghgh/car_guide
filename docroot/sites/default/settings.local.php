392a580e7b6178f655d1c117f8d632c3ee905c94
Shara Jamei <shara.jamei@adelphi.digital>
2018-07-08 11:43:12 +1000

    add local setting

---
+++ docroot/sites/default/settings.local.php
@@ -0,0 +1,17 @@
+<?php
+
+$databases = array (
+    'default' =>
+    array (
+        'default' =>
+        array (
+            'database' => 'car_guide',
+            'username' => 'root',
+            'password' => '',
+            'host' => 'localhost',
+            'port' => '',
+            'driver' => 'mysql',
+            'prefix' => '',
+        ),
+    ),
+);
